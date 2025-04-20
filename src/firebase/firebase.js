25742b7f726f4621e20c7f3fdc61b110af0acba7
munduni anthony <mundunianthony@gmail.com>
2025-04-20 03:02:10 +0300

    chore: add Firebase initialization code

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyflZ6cKrDa2SmHMz5es3Oxn62u3fJQtI",
+  authDomain: "tempalert-7abb2.firebaseapp.com",
+  projectId: "tempalert-7abb2",
+  storageBucket: "tempalert-7abb2.firebasestorage.app",
+  messagingSenderId: "952450715967",
+  appId: "1:952450715967:web:558fc36f4eefb7da2ac38a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
