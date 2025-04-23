9e8bc9ef6be6407a90b9d05f0a3494ac09e58df4
munduni anthony <mundunianthony@gmail.com>
2025-04-23 16:03:14 +0300

    refactor: remove unused Firebase initialization code and configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,4 @@
-// src/lib/firebase.ts
-
-import { getApps, initializeApp } from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
-// Your Firebase “web” config; you only need this to register your app.
-// RNFirebase reads all other settings from native config files automatically.
-const firebaseConfig = {
-  apiKey: "AIzaSyCyflZ6cKrDa2SmHMz5es3Oxn62u3fJQtI",
-  authDomain: "tempalert-7abb2.firebaseapp.com",
-  projectId: "tempalert-7abb2",
-  storageBucket: "tempalert-7abb2.appspot.com",
-  messagingSenderId: "952450715967",
-  appId: "1:952450715967:web:558fc36f4eefb7da2ac38a",
-};
-
-// Prevent re-initializing if hot reloading
-if (!getApps().length) {
-  initializeApp(firebaseConfig);
-}
-
-// Export the “auth” and “db” instances for the rest of your app
 export { auth, firestore as database };
