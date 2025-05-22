58fb045e3f60f1cf530f9372f44113c413db3121
munduni anthony <mundunianthony@gmail.com>
2025-05-22 09:52:25 +0300

    remove redundant files and create firebase config file

---
+++ src/services/authservice.ts
@@ -0,0 +1,19 @@
+// src/services/authService.ts
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from './firebaseConfig';
+import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
+
+export const signUp = async (email: string, password: string) => {
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+  await setDoc(doc(db, 'users', user.uid), {
+    email: user.email,
+    createdAt: serverTimestamp(),
+  });
+  return user;
+};
+
+export const signIn = async (email: string, password: string) => {
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  return userCredential.user;
+};
